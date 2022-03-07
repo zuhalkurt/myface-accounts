23d6ce78b848faad346628b4272e44fd2efdf78e
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-03-07 18:47:24 +0000

    Move password Hash function to its own class

--- myface-api/MyFace/Helpers/UrlHelper.cs
+++ myface-api/MyFace/Helpers/UrlHelper.cs
@@ -6,24 +6,6 @@ namespace MyFace.Helpers
 {
     public static class UrlHelper
     {
-        public static string CreateHashValue(string password)
-        {
-                byte[] salt = new byte[128 / 8];
-                using (var rngCsp = new RNGCryptoServiceProvider())
-                  {
-                         rngCsp.GetNonZeroBytes(salt);
-                  }
-                    string saltString = Convert.ToBase64String(salt);
-
-            // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
-                string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-                password: password,
-                salt: salt,
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: 100000,
-                numBytesRequested: 256 / 8));
-
-            return hashed;
+        
     }
-        }
-    }
+}

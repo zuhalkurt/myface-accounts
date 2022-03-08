734cf22f8e2221f6b5aeabb471571347c5ab5d7d
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-03-08 13:34:19 +0000

    Authorization added

--- myface-api/MyFace/Controllers/PostsController.cs
+++ myface-api/MyFace/Controllers/PostsController.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyFace.Models.Request;
 using MyFace.Models.Response;
+using MyFace.Models.Database;
 using MyFace.Repositories;
+using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using Microsoft.AspNetCore.Http;
+using MyFace.Helpers;
 
 namespace MyFace.Controllers
 {
@@ -10,10 +16,12 @@ namespace MyFace.Controllers
     public class PostsController : ControllerBase
     {    
         private readonly IPostsRepo _posts;
+        private readonly IUsersRepo _users;
 
-        public PostsController(IPostsRepo posts)
+        public PostsController(IPostsRepo posts, IUsersRepo users)
         {
             _posts = posts;
+            _users = users;
         }
         
         [HttpGet("")]
@@ -39,6 +47,52 @@ namespace MyFace.Controllers
                 return BadRequest(ModelState);
             }
             
+            var authHeader = Request.Headers["Authorization"];
+
+            if(authHeader == StringValues.Empty)
+            {
+                return Unauthorized();
+            }
+
+            var authHeaderString = authHeader[0];
+            var authHeaderSplit = authHeaderString.Split(' ');
+            var authType = authHeaderSplit[0];
+            var encodedUsernamePassword = authHeaderSplit[1];
+
+            var usernamePassword = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+
+            var usernamePasswordArray = usernamePassword.Split(':');
+
+            var username = usernamePasswordArray[0];
+            var password = usernamePasswordArray[1];
+
+            User user;
+
+            try
+            {
+                user = _users.GetByUsername(username);
+            }
+
+            catch (InvalidOperationException e)
+            {
+                return Unauthorized("The username/password combination does not match");
+            }
+
+            string hashed = PasswordHelper.CreateHashValue(password, Convert.FromBase64String(user.Salt));
+
+            if(hashed != user.HashedPassword)
+            {
+                return Unauthorized("The username/password combination does not match");
+            }
+
+            if(user.Id != newPost.UserId)
+            {
+                return StatusCode(
+                    StatusCodes.Status403Forbidden,
+                    "You are not allowed to create a post for a different user"
+                );
+            }
+
             var post = _posts.Create(newPost);
 
             var url = Url.Action("GetById", new { id = post.Id });

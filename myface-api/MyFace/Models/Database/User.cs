a50401ae99d9beb9797b2bc40e0d5075e1ef53e2
Zuhal Kurt <Zuhal.Kurt@softwire.com>
2022-03-11 11:21:25 +0000

    Add Role to user.cs

--- myface-api/MyFace/Models/Database/User.cs
+++ myface-api/MyFace/Models/Database/User.cs
@@ -5,7 +5,7 @@ namespace MyFace.Models.Database
     public class User
     {
         public int Id { get; set; }
-
+        public string Role { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
@@ -14,6 +14,7 @@ namespace MyFace.Models.Database
         public string CoverImageUrl { get; set; }
         public string HashedPassword { get; set; }
         public string Salt { get; set; }
+
         public ICollection<Post> Posts { get; set; } = new List<Post>();
         public ICollection<Interaction> Interactions { get; set; } = new List<Interaction>();
     }
